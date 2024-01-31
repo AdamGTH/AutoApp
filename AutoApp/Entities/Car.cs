dff3abba10239c1766dc5f9afa2f9ec363fae026
AdamGTH <adamma25@poczta.fm>
2024-01-31 20:13:15 +0100

    usuniecie settera

--- AutoApp/Entities/Car.cs
+++ AutoApp/Entities/Car.cs
@@ -9,30 +9,9 @@ namespace AutoApp.Entities
 {
     public class Car : CarBase
     {
-        private string type = "Petrol";
         public string? BrandName { get; set; }
-        public string? TypeOfEngine { get { return this.type; }
-            set
-            {
-                
-                switch(value.ToLower())
-                {
-                    case "electric": 
-                        this.type = "Electric";
-                        break;
+        public string? TypeOfEngine { get; set; }
 
-                    case "hydrogen":
-                        this.type = "Hydrogen";
-                        break;
-
-                    default:
-                        this.type = "Petrol";
-                        break;
-
-                }
-            }
-                }
-
-        public override string ToString() => $"Id: {Id}, Name: {BrandName}, Fuel: {type}";
+        public override string ToString() => $"Id: {Id}, Name: {BrandName}, Fuel: {TypeOfEngine}";
     }
 }
