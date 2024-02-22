d8b39a850192455ba3f1ef371e74566fbad8b501
AdamGTH <adamma25@poczta.fm>
2024-02-22 20:26:11 +0100

    1 dodanie Dependency injection

---
+++ AutoApp/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoApp
+{
+    public class App : IApp
+    {
+        public void Run()
+        {
+            Console.WriteLine("Witaj w App !!!");
+        }
+    }
+}
--- AutoApp/Program.cs
+++ AutoApp/Program.cs
@@ -1,117 +1,127 @@
-﻿using AutoApp.Data;
+﻿using AutoApp;
+using AutoApp.Data;
 using AutoApp.Entities;
 using AutoApp.Repositories;
+using Microsoft.Extensions.DependencyInjection;
 using System.Diagnostics;
 using System.Reflection;
 using System.Text.Json;
 
-var CarRepository = new RepositoryInFile<Car>();
-CarRepository.GetAll();
-CarRepository.ItemAdded += EventAdded;
-CarRepository.ItemDeleted += EventDeleted;
+var services = new ServiceCollection();
+services.AddSingleton<IApp,  App>();
 
-var num = "";
-Console.WriteLine("|*----------------------MAIN MENU---------------------*|");
-while (num != "q")
-{
-    switch (num)
-    {
-        case "1": AddCar(CarRepository); break;
-        case "2": RemoveCar(CarRepository); break;
-        case "3": WriteAllToConsole(CarRepository); break;
-        case "q": break;
-        default: break;
-    }
-    Console.WriteLine();
-    Console.WriteLine("-1- ADD CAR ");
-    Console.WriteLine("-2- DELETE CAR ");
-    Console.WriteLine("-3- READ ALL CARS ");
-    Console.WriteLine("-q- END ");
+var serviceProvider = services.BuildServiceProvider();
+var app = serviceProvider.GetService<IApp>();
 
-    num = Console.ReadLine();
-}
+app.Run();
 
-Console.WriteLine("|*-------------------THANK YOU FOR COMING--------------------*|");
+//var CarRepository = new RepositoryInFile<Car>();
+//CarRepository.GetAll();
+//CarRepository.ItemAdded += EventAdded;
+//CarRepository.ItemDeleted += EventDeleted;
 
-static void AddCar(IRepository<Car> carRepository)
-{
-    Console.WriteLine("|*-------------------ADD CAR TO FILE--------------------*|");
-    Console.WriteLine("BRAND: ");
-    var name = Console.ReadLine();
-    Console.WriteLine("TYPE OF FUEL: ");
-    var fuel = Console.ReadLine();
-    carRepository.Add(new Car {BrandName = name, TypeOfEngine = fuel }) ;
-    carRepository.Save();
-    Console.WriteLine();
-    Console.WriteLine("|*----------------------MAIN MENU---------------------*|");
-}
+//var num = "";
+//Console.WriteLine("|*----------------------MAIN MENU---------------------*|");
+//while (num != "q")
+//{
+//    switch (num)
+//    {
+//        case "1": AddCar(CarRepository); break;
+//        case "2": RemoveCar(CarRepository); break;
+//        case "3": WriteAllToConsole(CarRepository); break;
+//        case "q": break;
+//        default: break;
+//    }
+//    Console.WriteLine();
+//    Console.WriteLine("-1- ADD CAR ");
+//    Console.WriteLine("-2- DELETE CAR ");
+//    Console.WriteLine("-3- READ ALL CARS ");
+//    Console.WriteLine("-q- END ");
 
-static void RemoveCar(IRepository<Car> carRepository)
-{
-    Console.WriteLine("|*-------------------REMOVE MENU--------------------*|");
-    var items = carRepository.GetAll();
+//    num = Console.ReadLine();
+//}
 
-    foreach (var item in items)
-    {
-        Console.WriteLine(item);
-    }
+//Console.WriteLine("|*-------------------THANK YOU FOR COMING--------------------*|");
 
-    Console.WriteLine("SET NUMBER TO REMOVE OR 'q' ");
-    var num = Console.ReadLine();
+//static void AddCar(IRepository<Car> carRepository)
+//{
+//    Console.WriteLine("|*-------------------ADD CAR TO FILE--------------------*|");
+//    Console.WriteLine("BRAND: ");
+//    var name = Console.ReadLine();
+//    Console.WriteLine("TYPE OF FUEL: ");
+//    var fuel = Console.ReadLine();
+//    carRepository.Add(new Car {BrandName = name, TypeOfEngine = fuel }) ;
+//    carRepository.Save();
+//    Console.WriteLine();
+//    Console.WriteLine("|*----------------------MAIN MENU---------------------*|");
+//}
 
-    if (int.TryParse(num, out var result))
-    {
-        carRepository.Remove(carRepository.GetById(result));
-        carRepository.Save();
-    }
-    else
-    {
-        if (num == "q") 
-        {
-            Console.WriteLine();
-            Console.WriteLine("|*----------------------MAIN MENU---------------------*|"); 
-            return; 
-        }
-        Console.WriteLine("SOMETHING BROKE !");
-    }
+//static void RemoveCar(IRepository<Car> carRepository)
+//{
+//    Console.WriteLine("|*-------------------REMOVE MENU--------------------*|");
+//    var items = carRepository.GetAll();
 
-}
+//    foreach (var item in items)
+//    {
+//        Console.WriteLine(item);
+//    }
 
-static void WriteAllToConsole(IReadRepository<IEntity> repository)
-{
-    Console.WriteLine("|*-------------------ALL ITEMS--------------------*|");
-    var items = repository.GetAll();
+//    Console.WriteLine("SET NUMBER TO REMOVE OR 'q' ");
+//    var num = Console.ReadLine();
 
-    foreach (var item in items)
-    {
-        Console.WriteLine(item);
-    }
-    Console.WriteLine("q - wyjście");
-    var num = Console.ReadLine();
-    if (num == "q")
-    {
-        Console.WriteLine();
-        Console.WriteLine("|*----------------------MAIN MENU---------------------*|");
-        return;
-    }
-}
+//    if (int.TryParse(num, out var result))
+//    {
+//        carRepository.Remove(carRepository.GetById(result));
+//        carRepository.Save();
+//    }
+//    else
+//    {
+//        if (num == "q") 
+//        {
+//            Console.WriteLine();
+//            Console.WriteLine("|*----------------------MAIN MENU---------------------*|"); 
+//            return; 
+//        }
+//        Console.WriteLine("SOMETHING BROKE !");
+//    }
 
-void EventAdded(object? sender, Car e)
-{
-    DateTime dateTime = DateTime.Now;
-    Console.WriteLine($"{e.BrandName} added from {sender.GetType().Name}");
-    using (var writer = File.AppendText("logs.txt"))
-    {
-        writer.WriteLine($"[{dateTime}]-ItemAdded-[{e.BrandName}]");
-    }
-}
+//}
 
-void EventDeleted(object? sender, Car e)
-{
-    DateTime dateTime = DateTime.Now;
-    Console.WriteLine($"{e.BrandName} deleted from {sender.GetType().Name}");
-    using (var writer = File.AppendText("logs.txt"))
-    {
-        writer.WriteLine($"[{dateTime}]-ItemDeleted-[{e.BrandName}]");
-    }
-}
+//static void WriteAllToConsole(IReadRepository<IEntity> repository)
+//{
+//    Console.WriteLine("|*-------------------ALL ITEMS--------------------*|");
+//    var items = repository.GetAll();
+
+//    foreach (var item in items)
+//    {
+//        Console.WriteLine(item);
+//    }
+//    Console.WriteLine("q - wyjście");
+//    var num = Console.ReadLine();
+//    if (num == "q")
+//    {
+//        Console.WriteLine();
+//        Console.WriteLine("|*----------------------MAIN MENU---------------------*|");
+//        return;
+//    }
+//}
+
+//void EventAdded(object? sender, Car e)
+//{
+//    DateTime dateTime = DateTime.Now;
+//    Console.WriteLine($"{e.BrandName} added from {sender.GetType().Name}");
+//    using (var writer = File.AppendText("logs.txt"))
+//    {
+//        writer.WriteLine($"[{dateTime}]-ItemAdded-[{e.BrandName}]");
+//    }
+//}
+
+//void EventDeleted(object? sender, Car e)
+//{
+//    DateTime dateTime = DateTime.Now;
+//    Console.WriteLine($"{e.BrandName} deleted from {sender.GetType().Name}");
+//    using (var writer = File.AppendText("logs.txt"))
+//    {
+//        writer.WriteLine($"[{dateTime}]-ItemDeleted-[{e.BrandName}]");
+//    }
+//}
