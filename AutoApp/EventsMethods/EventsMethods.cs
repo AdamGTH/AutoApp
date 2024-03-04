5e089c0318beb4e6fff5fb9e83c4983bde523304
AdamGTH <adamma25@poczta.fm>
2024-03-04 18:50:35 +0100

    add events methods

---
+++ AutoApp/EventsMethods/EventsMethods.cs
@@ -0,0 +1,35 @@
+﻿using AutoApp.Entities;
+using AutoApp.Events;
+using AutoApp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoApp.EventsMethods
+{
+    public class EventsMethods : IEventsMethods
+    {
+       
+        public void EventAdded(object? sender, Car e)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine($"{e.BrandName} added from {sender.GetType().Name}");
+            using (var writer = File.AppendText("logs.txt"))
+            {
+                writer.WriteLine($"[{dateTime}]-ItemAdded-[{e.BrandName}]");
+            }
+        }
+
+        public void EventDeleted(object? sender, Car e)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine($"{e.BrandName} deleted from {sender.GetType().Name}");
+            using (var writer = File.AppendText("logs.txt"))
+            {
+                writer.WriteLine($"[{dateTime}]-ItemDeleted-[{e.BrandName}]");
+            }
+        }
+    }
+}
---
+++ AutoApp/EventsMethods/IEventsMethods.cs
@@ -0,0 +1,16 @@
+﻿using AutoApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoApp.Events
+{
+    public interface IEventsMethods
+    {
+        void EventAdded(object? sender, Car e);
+        void EventDeleted(object? sender, Car e);
+
+    }
+}
